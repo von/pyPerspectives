d496982cba98d4209ab4c76e78f737d0c07c4154
Von Welch <von@vwelch.com>
2011-09-10 18:18:17 -0400

    Notaries: Make class of Notary list member an attribute

--- Perspectives/Notary.py
+++ Perspectives/Notary.py
@@ -24,117 +24,6 @@ from HTTP_dispatcher import HTTP_dispatcher
 
 import timed_asyncore
 
-class Notaries(list):
-    """Class for representing the set of trusted Notaries"""
-
-    def __init__(self):
-        self.logger = logging.getLogger("Perspectives.Notary")
-        list.__init__(self)
-
-    @classmethod
-    def default(cls):
-        """Return the default Notaries"""
-        data = pkgutil.get_data("Perspectives", "conf/http_notary_list.txt")
-        return cls.from_stream(StringIO.StringIO(data))
-
-    @classmethod
-    def from_file(cls, file_path):
-        """Return Notaries described in file.
-
-        See from_stream() for expected format."""
-        with file(file_path, "r") as f:
-            notaries = cls.from_stream(f)
-        return notaries
-
-    @classmethod
-    def from_stream(cls, stream):
-        """Return Notaries described in given stream.
-
-        Expected format for each Notary is:
-        # Lines starting with '#' are comments and ignored
-        <hostname>:<port>
-        -----BEGIN PUBLIC KEY-----
-        <multiple lines of Base64-encoded data>
-        -----END PUBLIC KEY----
-        """
-        notaries = Notaries()
-        while True:
-            notary = Notary.from_stream(stream)
-            if notary is None:  # EOF
-                break
-            else:
-                notaries.append(notary)
-        return notaries
-
-    def query(self, service, num=0, timeout=10):
-        """Query Notaries and return NotaryResponses instance
-
-        For any Notary not responding, a None will be in the array.
-
-        num specifies the number of Notaries to query. If 0, all notaries
-        are queried.
-
-        timeout is the timeout in seconds"""
-        if num == 0:
-            to_query = self
-        else:
-            if num > len(self):
-                raise ValueError("Too many notaries requested (%s > %s)" % (num, len(self)))
-            to_query = random.sample(self, num)
-        responses = NotaryResponses()
-        dispatchers = []
-        # Use own map here for thread safety
-        map = {}
-        for notary in to_query:
-            self.logger.debug("Querying %s about %s..." % (notary, service))
-            dispatchers.append((notary,
-                                HTTP_dispatcher(notary.get_url(service),
-                                                map=map)))
-        self.logger.debug("Calling asyncore.loop()")
-        timed_asyncore.loop_with_timeout(timeout=timeout, map=map)
-        self.logger.debug("asyncore.loop() done.")
-        for notary, dispatcher in dispatchers:
-            try:
-                response = dispatcher.get_response()
-                xml = response.read()
-                self.logger.debug("Validating response from %s (%d bytes)" % (notary, len(xml)))
-                response = NotaryResponse(xml)
-                notary.verify_response(response, service)
-                self.logger.debug("Response signature verified")
-                responses.append(response)
-            except EOFError as e:
-                self.logger.error("Failed to get response from %s: %s" % (notary, str(e)))
-                responses.append(None)
-            except httplib.BadStatusLine as e:
-                self.logger.error("Failed to parse response from %s, bad status: %s" % (notary, e))
-                responses.append(None)
-            except NotaryException as e:
-                self.logger.error("Error validating response from %s: %s" % (notary, e))
-                responses.append(None)
-            except Exception as e:
-                self.logger.error("Unknown error handling response from %s: %s" % (notary, e))
-                responses.append(None)
-        return responses
-
-    def find_notary(self, hostname, port=None):
-        """Find notary inlist.
-
-        hostname must match. If port is not None, it must match too.
-
-        Returns None if notary is not found."""
-        for notary in self:
-            if notary.hostname != hostname:
-                continue
-            if (port is not None) and (notary.port != port):
-                continue
-            return notary
-        # Failure
-        return None
-
-    def __str__(self):
-        return "[" + ",".join([str(n) for n in self]) + "]"
-
-
 class Notary:
     """Class for representing Perspective Notary"""
 
@@ -211,7 +100,7 @@ class Notary:
             return None
         if public_key is None:
             raise NotaryException("No public key found for Notary %s:%s" % (hostname, port))
-        return Notary(hostname, port, public_key)
+        return cls(hostname, port, public_key)
 
     @classmethod
     def _public_key_from_lines(cls, lines):
@@ -249,6 +138,119 @@ class Notary:
             raise NotaryResponseBadSignature("Signature verification failed")
         elif result != 1:
             raise NotaryResponseException("Error verifying signature")
+
+class Notaries(list):
+    """Class for representing the set of trusted Notaries"""
+
+    NotaryClass = Notary
+
+    def __init__(self):
+        self.logger = logging.getLogger("Perspectives.Notary")
+        list.__init__(self)
+
+    @classmethod
+    def default(cls):
+        """Return the default Notaries"""
+        data = pkgutil.get_data("Perspectives", "conf/http_notary_list.txt")
+        return cls.from_stream(StringIO.StringIO(data))
+
+    @classmethod
+    def from_file(cls, file_path):
+        """Return Notaries described in file.
+
+        See from_stream() for expected format."""
+        with file(file_path, "r") as f:
+            notaries = cls.from_stream(f)
+        return notaries
+
+    @classmethod
+    def from_stream(cls, stream):
+        """Return Notaries described in given stream.
+
+        Expected format for each Notary is:
+        # Lines starting with '#' are comments and ignored
+        <hostname>:<port>
+        -----BEGIN PUBLIC KEY-----
+        <multiple lines of Base64-encoded data>
+        -----END PUBLIC KEY----
+        """
+        notaries = cls()
+        while True:
+            notary = cls.NotaryClass.from_stream(stream)
+            if notary is None:  # EOF
+                break
+            else:
+                notaries.append(notary)
+        return notaries
+
+    def query(self, service, num=0, timeout=10):
+        """Query Notaries and return NotaryResponses instance
+
+        For any Notary not responding, a None will be in the array.
+
+        num specifies the number of Notaries to query. If 0, all notaries
+        are queried.
+
+        timeout is the timeout in seconds"""
+        if num == 0:
+            to_query = self
+        else:
+            if num > len(self):
+                raise ValueError("Too many notaries requested (%s > %s)" % (num, len(self)))
+            to_query = random.sample(self, num)
+        responses = NotaryResponses()
+        dispatchers = []
+        # Use own map here for thread safety
+        map = {}
+        for notary in to_query:
+            self.logger.debug("Querying %s about %s..." % (notary, service))
+            dispatchers.append((notary,
+                                HTTP_dispatcher(notary.get_url(service),
+                                                map=map)))
+        self.logger.debug("Calling asyncore.loop()")
+        timed_asyncore.loop_with_timeout(timeout=timeout, map=map)
+        self.logger.debug("asyncore.loop() done.")
+        for notary, dispatcher in dispatchers:
+            try:
+                response = dispatcher.get_response()
+                xml = response.read()
+                self.logger.debug("Validating response from %s (%d bytes)" % (notary, len(xml)))
+                response = NotaryResponse(xml)
+                notary.verify_response(response, service)
+                self.logger.debug("Response signature verified")
+                responses.append(response)
+            except EOFError as e:
+                self.logger.error("Failed to get response from %s: %s" % (notary, str(e)))
+                responses.append(None)
+            except httplib.BadStatusLine as e:
+                self.logger.error("Failed to parse response from %s, bad status: %s" % (notary, e))
+                responses.append(None)
+            except NotaryException as e:
+                self.logger.error("Error validating response from %s: %s" % (notary, e))
+                responses.append(None)
+            except Exception as e:
+                self.logger.error("Unknown error handling response from %s: %s" % (notary, e))
+                responses.append(None)
+        return responses
+
+    def find_notary(self, hostname, port=None):
+        """Find notary inlist.
+
+        hostname must match. If port is not None, it must match too.
+
+        Returns None if notary is not found."""
+        for notary in self:
+            if notary.hostname != hostname:
+                continue
+            if (port is not None) and (notary.port != port):
+                continue
+            return notary
+        # Failure
+        return None
+
+    def __str__(self):
+        return "[" + ",".join([str(n) for n in self]) + "]"
+
 
 class NotaryResponses(list):
     """Wrapper around a list of NotaryResponse instances"""
