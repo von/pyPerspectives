83def16e08b445454d48128d321b3cd0e46b5b6b
Von Welch <von@vwelch.com>
2011-12-11 18:48:34 -0500

    Added Service.get_fingerprint()

--- Perspectives/Service.py
+++ Perspectives/Service.py
@@ -1,5 +1,10 @@
 """Classes for representing application services"""
 
+import re
+import ssl
+
+from Fingerprint import Fingerprint
+
 class Service:
     """Representation of a application service"""
 
@@ -13,6 +18,18 @@ class Service:
 
     def __str__(self):
         return "%s:%s,%s" % (self.hostname, self.port, self.type)
+
+    # ssl.get_server_certificate() doesn't terminate the PEM correctly,
+    # it is missing a newline before the END line. This re detects that.
+    bad_cert_end_re = re.compile("(\S)(-----END CERTIFICATE-----)$",
+                                 re.MULTILINE)
+
+    def get_fingerprint(self):
+        """Return the Service's certificate fingerprint"""
+        cert_pem = ssl.get_server_certificate((self.hostname, self.port))
+        # Fix missing newline before END line
+        cert_pem = self.bad_cert_end_re.sub(r"\1\n\2", cert_pem)
+        return Fingerprint.from_certificate_PEM(cert_pem)
 
 class ServiceType:
     """Constants for service types"""
